e0bc588760f989d7ba34a99d4b28b328619d5b2b
AZU-URE <202051069@iiitvadodara.ac.in>
2023-03-24 10:10:33 +0530

    Added reset

--- Vsync/Vsync/settings.py
+++ Vsync/Vsync/settings.py
@@ -1,3 +1,4 @@
+import environ
 """
 Django settings for Vsync project.
 
@@ -12,6 +13,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)lko5=er+ta#s=io)1*0x93pcfdqlp(uev0&evf%a*@2#*=f#%'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
